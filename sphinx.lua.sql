645e3fd41d8ac09243a3b627951ea1760639a870
Mikhail Che <ernesto.chege@yandex.ru>
2016-05-04 16:27:22 +0500

    lua mysql

--- sphinx.lua.sql
+++ sphinx.lua.sql
@@ -2,8 +2,18 @@ do $$
 -- load driver
 local driver = require "luasql.mysql"
 -- create environment object
-env = assert (driver.mysql())
+local env = assert (driver.mysql())
 -- connect to data source
-con = assert (env:connect("",nil,nil,'127.0.0.1', 9306))
-print(con)
+local con = assert (env:connect("",nil,nil,'127.0.0.1', 9306))
+--print(con)
+-- retrieve a cursor
+local cur = assert (con:execute("SELECT *,weight() FROM idx1 WHERE MATCH('алла') LIMIT 10"))
+local row = {}
+repeat
+  print(row)
+until row = cur:fetch (row, "a")
+-- close everything
+cur:close() -- already closed because all the result set was consumed
+con:close()
+env:close()
 $$ language plluau;
